04e79be4ed6df022b9e5761ef79324c90a19eb44
charlesfeatherstone <charlesfstone11@gmail.com>
2023-09-25 12:31:20 -0400

    added db.json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch(imgUrl)
+.then(function(response)
+{
+    
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+    const array=data.message
+    console.log(array)
+    array.forEach((image) => {
+        console.log(image)
+        const div=document.createElement('div')
+        const img=document.createElement('img')
+        const imagePlace=document.getElementById('dog-image-container')
+        img.src=image
+        imagePlace.append(div)
+        div.append(img)
+    });
+})
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+fetch(breedUrl)
+.then(function(response)
+{
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+    const array=Object.keys(data.message)
+    console.log(array)
+    array.forEach((dog) => {
+        console.log(dog)
+        const div=document.createElement('div')
+        const p=document.createElement('p')
+        const ul=document.getElementById('dog-breeds')
+        p.innerText=dog
+        ul.append(div)
+        div.append(p)
+    });
+})
+// object.keys gives an array of object keys and you can call for each on it
